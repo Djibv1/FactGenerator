ef2715129840b17b21f6a00b3a7dfc2948e8ceae
Djibv1 <djilanibourhan@yahoo.fr>
2025-10-17 21:24:39 +0200

    responsive maj

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const content = document.getElementById("content");
 const source = document.getElementById("source");
 
-console.log(source);
-
-function getJoke() {
+function getFact() {
   fetch("https://uselessfacts.jsph.pl/random.json")
     .then((res) => res.json())
     .then((data) => {
       const fact = data.text;
       const url = data.source_url;
-      console.log(source);
-
       content.textContent = fact;
       source.href = url;
     });
 }
-getJoke();
-document.body.addEventListener("click", getJoke);
+getFact();
+document.body.addEventListener("click", getFact);
